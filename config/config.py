cc9e1281c2c2f4f9d368cfbbb09f39866be24bda
Кустов Михаил Дмитриевич <KustovMD@MACT.RU>
2026-06-16 11:55:06 +0300

    Changes to test.yml

--- config/config.py
+++ config/config.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    APPHOST = os.getenv("APPIUM_HOST", "127.0.0.1")
-    APPPORT = os.getenv("APPIUM_PORT", "4808")
-    UDID = os.getenv("DEVICE_UDID", "4929312")
+    APPHOST = "127.0.0.1"
+    APPPORT ="4808"
+    UDID = "4929312"
 
     MOBILE_PACKAGE = "com.dmc_mobile"
     MOBILE_ACTIVITY = ".MainActivity"
--- tests/test_code_information.py
+++ tests/test_code_information.py
@@ -10,6 +10,5 @@ class TestCodeInformation:
     @allure.title("DMC Mobile. Получение информации по коду. Сканирование не агрегированного КМ")
     def test_barcode_scan_simulation(self, code_information_page):
         code_information_page.emulate_scan("0107665585002196215Ut,r7FhgAHj.93dGVz")
-        time.sleep(5)
 
 
