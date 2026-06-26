83d2483e3072c4ee88e0ed0568839d1e6f431103
Кустов Михаил Дмитриевич <KustovMD@MACT.RU>
2026-06-26 17:49:11 +0300

    6 рабочих автотестов + ci + testit(Тест 10)

--- utils/step_utils.py
+++ utils/step_utils.py
@@ -1,10 +1,17 @@
 import allure
 import testit
 
+import functools
+
 def shared_step(name: str):
-    with allure.step(name):
-        with testit.step(name):
-            yield
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with allure.step(name):
+                with testit.step(name):
+                    return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 def shared_title(name: str):
     def decorator(func):
